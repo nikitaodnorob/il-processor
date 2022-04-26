dcd8ff9e3a993d4b9a17c5c6f275cf706a795a7c
Nikita Odnorob <nodnorob@tradingview.com>
2022-04-27 00:15:57 +0300

    Add cluster analysis

---
+++ ClusterAnalysis/ILCodes.cs
@@ -0,0 +1,92 @@
+namespace ClusterAnalysis;
+
+public static class ILCodes
+{
+    public static readonly List<string> Files = new List<string>
+    {
+        "01_ulearn_rectangles/author1/my_release.il",
+        "01_ulearn_rectangles/author1-net5/my_release.il",
+        "01_ulearn_rectangles/author1-netcore3/my_release.il",
+        "01_ulearn_rectangles/author2/my_release.il",
+        "01_ulearn_rectangles/author2-net5/my_release.il",
+        "01_ulearn_rectangles/author2-netcore3/my_release.il",
+        
+        "02_ulearn_pluralize/author1/my_release.il",
+        "02_ulearn_pluralize/author1-net5/my_release.il",
+        "02_ulearn_pluralize/author1-netcore3/my_release.il",
+        "02_ulearn_pluralize/author2/my_release.il",
+        "02_ulearn_pluralize/author2-net5/my_release.il",
+        "02_ulearn_pluralize/author2-netcore3/my_release.il",
+        "02_ulearn_pluralize/author3/my_release.il",
+        "02_ulearn_pluralize/author3-net5/my_release.il",
+        "02_ulearn_pluralize/author3-netcore3/my_release.il",
+        
+        "03_ulearn_distance/author1/my_release.il",
+        "03_ulearn_distance/author1-net5/my_release.il",
+        "03_ulearn_distance/author1-netcore3/my_release.il",
+        "03_ulearn_distance/author2/my_release.il",
+        "03_ulearn_distance/author2-net5/my_release.il",
+        "03_ulearn_distance/author2-netcore3/my_release.il",
+        "03_ulearn_distance/author3/my_release.il",
+        "03_ulearn_distance/author3-net5/my_release.il",
+        "03_ulearn_distance/author3-netcore3/my_release.il",
+        
+        "04_ulearn_mazes/author1/my_release.il",
+        "04_ulearn_mazes/author1-net5/my_release.il",
+        "04_ulearn_mazes/author1-netcore3/my_release.il",
+        "04_ulearn_mazes/author2/my_release.il",
+        "04_ulearn_mazes/author2-net5/my_release.il",
+        "04_ulearn_mazes/author2-netcore3/my_release.il",
+        "04_ulearn_mazes/author3/my_release.il",
+        "04_ulearn_mazes/author3-net5/my_release.il",
+        "04_ulearn_mazes/author3-netcore3/my_release.il",
+        
+        "05_ulearn_names/author1/my_release.il",
+        "05_ulearn_names/author2/my_release.il",
+        "05_ulearn_names/author3/my_release.il",
+        "05_ulearn_names/author4/my_release.il",
+        "05_ulearn_names/author1-refactored/my_release.il",
+        "05_ulearn_names/author2-refactored/my_release.il",
+        "05_ulearn_names/author3-refactored/my_release.il",
+        "05_ulearn_names/author4-refactored/my_release.il",
+        
+        "06_ulearn_text_analysis/author1/my_release.il",
+        "06_ulearn_text_analysis/author2/my_release.il",
+        "06_ulearn_text_analysis/author3/my_release.il",
+        "06_ulearn_text_analysis/author4/my_release.il",
+        
+        "07_ulearn_images/author1/my_release.il",
+        "07_ulearn_images/author2/my_release.il",
+        "07_ulearn_images/author3/my_release.il",
+        "07_ulearn_images/author4/my_release.il",
+        
+        "09_ulearn_autocomplete/author1/my_debug.il",
+        "09_ulearn_autocomplete/author2/my_debug.il",
+        "09_ulearn_autocomplete/author4/my_debug.il",
+        
+        "09_ulearn_autocomplete/author1/my_release.il",
+        "09_ulearn_autocomplete/author2/my_release.il",
+        "09_ulearn_autocomplete/author4/my_release.il",
+        
+        "11_ulearn_antiplagiat/author1/my_debug.il",
+        "11_ulearn_antiplagiat/author2/my_debug.il",
+        "11_ulearn_antiplagiat/author4/my_debug.il",
+        
+        "11_ulearn_antiplagiat/author1/my_release.il",
+        "11_ulearn_antiplagiat/author2/my_release.il",
+        "11_ulearn_antiplagiat/author4/my_release.il",
+        
+        "12_ulearn_gauss/author1/my_debug.il",
+        "12_ulearn_gauss/author3/my_debug.il",
+        "12_ulearn_gauss/author4/my_debug.il",
+        
+        "12_ulearn_gauss/author1/my_release.il",
+        "12_ulearn_gauss/author3/my_release.il",
+        "12_ulearn_gauss/author4/my_release.il",
+        
+        "codewars_duration_format/author1/my_release_net_framework_4_7_2.il",
+        "codewars_duration_format/author1/my_release_net_core_2_1.il",
+        "codewars_duration_format/author1/my_release_net_core_3_1.il",
+        "codewars_duration_format/author1/my_release_net_5_0.il",
+    };
+}
---
+++ ClusterAnalysis/Program.cs
@@ -0,0 +1,262 @@
+﻿using System.Numerics;
+using ILLexer;
+
+namespace ClusterAnalysis;
+
+static class Program
+{
+    private static readonly Dictionary<string, List<Lexeme>> Codes = new();
+    private static readonly Dictionary<string, double[]> LexemesBag = new();
+
+    private const double MaxClustersDistance = 0.05;
+    
+    public static void Main(string[] args)
+    {
+        var clusters = new List<List<string>>();
+        
+        foreach (string fileName in ILCodes.Files)
+        {
+            string ilCode = File.ReadAllText($"../../../../../master-diploma/{fileName}");
+            Console.WriteLine($"Processing {fileName}");
+            var fileLexemes = Lexer.GetLexemes(ilCode);
+            Codes.Add(fileName, CorrectFileLexemes(fileLexemes));
+            clusters.Add(new List<string> {fileName});
+        }
+        Console.WriteLine("==================");
+
+        CountLexemesBag();
+
+        while (true)
+        {
+            var nearestClusters = GetNearestClusters(clusters);
+            if (nearestClusters == null) break;
+
+            var (clusterA, clusterB) = ((List<string>, List<string>))nearestClusters;
+
+            clusterA.AddRange(clusterB);
+            clusters.Remove(clusterB);
+        }
+
+        foreach (var cluster in clusters)
+        {
+            cluster.ForEach(Console.WriteLine);
+            Console.WriteLine("-------------------");
+        }
+
+        int rightOneClusterCnt = 0;
+        int wrongOneClusterCnt = 0;
+        int wrongDifferentClustersCnt = 0;
+        int rightDifferentClustersCnt = 0;
+        
+        // check accuracy
+        for (int i = 0; i < ILCodes.Files.Count; i++)
+        {
+            for (int j = i + 1; j < ILCodes.Files.Count; j++)
+            {
+                string fileName1 = ILCodes.Files[i];
+                string fileName2 = ILCodes.Files[j];
+                int author1 = int.Parse(fileName1.Substring(
+                    fileName1.IndexOf("/author", StringComparison.Ordinal) + 7,
+                    1
+                ));
+                int author2 = int.Parse(fileName2.Substring(
+                    fileName2.IndexOf("/author", StringComparison.Ordinal) + 7,
+                    1
+                ));
+                var cluster1 = clusters.Find(c => c.Contains(fileName1));
+                var cluster2 = clusters.Find(c => c.Contains(fileName2));
+
+                if (author1 == author2 && cluster1 == cluster2)
+                {
+                    // Console.ForegroundColor = ConsoleColor.Green;
+                    // Console.WriteLine($"{fileName1} {fileName2}");
+                    // Console.ResetColor();
+                    rightOneClusterCnt++;
+                }
+                else if (author1 != author2 && cluster1 == cluster2)
+                {
+                    // Console.ForegroundColor = ConsoleColor.Red;
+                    // Console.WriteLine($"{fileName1} {fileName2}");
+                    // Console.ResetColor();
+                    wrongOneClusterCnt++;
+                }
+                else if (author1 == author2 && cluster1 != cluster2)
+                {
+                    // Console.ForegroundColor = ConsoleColor.Yellow;
+                    // Console.WriteLine($"{fileName1} {fileName2}");
+                    // Console.ResetColor();
+                    wrongDifferentClustersCnt++;
+                }
+                else if (author1 != author2 && cluster1 != cluster2)
+                {
+                    rightDifferentClustersCnt++;
+                }
+            }
+        }
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("\n============\n");
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"N1 (really one cluster) = {rightOneClusterCnt}");
+        Console.WriteLine($"N2 (wrong one cluster) = {wrongOneClusterCnt}");
+        Console.WriteLine($"N3 (wrong different clusters) = {wrongDifferentClustersCnt}");
+        Console.WriteLine($"N4 (really different clusters) = {rightDifferentClustersCnt}");
+
+        double accuracy = (6d * rightOneClusterCnt - 4d * wrongOneClusterCnt - 2d * wrongDifferentClustersCnt + rightDifferentClustersCnt) /
+                          (5 * ILCodes.Files.Count * ILCodes.Files.Count) + 0.4;
+        Console.WriteLine($"Accuracy = {accuracy}");
+    }
+
+    private static (List<string>, List<string>)? GetNearestClusters(List<List<string>> clusters)
+    {
+        (List<string>?, List<string>?, double) nearestClusters = (null, null, double.PositiveInfinity);
+
+        for (int i = 0; i < clusters.Count; i++)
+        {
+            for (int j = i + 1; j < clusters.Count; j++)
+            {
+                double distance = GetClustersDistance(clusters[i], clusters[j]);
+                if (distance < nearestClusters.Item3)
+                {
+                    nearestClusters = (clusters[i], clusters[j], distance);
+                }
+            }
+        }
+
+        if (nearestClusters.Item3 > MaxClustersDistance) return null;
+
+        return (nearestClusters.Item1, nearestClusters.Item2);
+    }
+
+    private static double GetClustersDistance(List<string> clusterA, List<string> clusterB)
+    {
+        double distanceSum = 0;
+
+        foreach (string fileA in clusterA)
+        {
+            foreach (string fileB in clusterB)
+            {
+                distanceSum += GetCodesDistance(Codes[fileA], Codes[fileB], fileA, fileB);
+            }
+        }
+
+        return distanceSum / clusterA.Count / clusterB.Count;
+    }
+
+    private static double GetCodesDistance(List<Lexeme> codeA, List<Lexeme> codeB, string fileNameA, string fileNameB)
+    {
+        // return GetCodesJaccardDistance(codeA, codeB);
+        return GetCodesCosDistance(fileNameA, fileNameB);
+    }
+
+    private static double GetCodesJaccardDistance(List<Lexeme> codeA, List<Lexeme> codeB)
+    {
+        var codeALexemes = new HashSet<string>();
+        var codeBLexemes = new HashSet<string>();
+        codeA.ForEach(lexeme => codeALexemes.Add(lexeme.LexemeText));
+        codeB.ForEach(lexeme => codeBLexemes.Add(lexeme.LexemeText));
+        var codeABLexemes = codeALexemes.Intersect(codeBLexemes);
+
+        int codeABLexemesCount = codeABLexemes.Count();
+
+        return 1d - 1d * codeABLexemesCount / (codeALexemes.Count + codeBLexemes.Count - codeABLexemesCount);
+    }
+
+    private static void CountLexemesBag()
+    {
+        var lexemes = new HashSet<string>();
+        foreach (var code in Codes)
+        {
+            code.Value.ForEach(lexeme =>
+            {
+                if (lexeme.LexemeText.Length > 0) lexemes.Add(lexeme.LexemeText);
+            });
+        }
+
+        var idf = new Dictionary<string, double>();
+        foreach (var lexemeText in lexemes)
+        {
+            int codesCnt = Codes.Count(
+                code => code.Value.Select(lexeme => lexeme.LexemeText).Contains(lexemeText)
+            );
+            idf[lexemeText] = 1d + Math.Log(1d * Codes.Count / codesCnt);
+            // if (idf[lexemeText] < 1.3) Console.WriteLine(lexemeText);
+        }
+
+        int allLexemesCnt = lexemes.Count;
+        foreach (var code in Codes)
+        {
+            LexemesBag.Add(code.Key, new double[allLexemesCnt]);
+            int lexemeI = 0;
+            foreach (var lexemeText in lexemes)
+            {
+                // count
+                LexemesBag[code.Key][lexemeI] = code.Value.Count(lexeme => lexeme.LexemeText == lexemeText);
+                // tf
+                LexemesBag[code.Key][lexemeI] /= Codes[code.Key].Count;
+                // idf
+                LexemesBag[code.Key][lexemeI] *= idf[lexemeText];
+                lexemeI++;
+            }
+        }
+    }
+
+    private static double GetCodesCosDistance(string fileNameA, string fileNameB)
+    {
+        var vecA = LexemesBag[fileNameA];
+        var vecB = LexemesBag[fileNameB];
+
+        double dotProd = 0;
+        double sqA = 0;
+        double sqB = 0;
+        for (int i = 0; i < vecA.Length; i++)
+        {
+            dotProd += vecA[i] * vecB[i];
+            sqA += vecA[i] * vecA[i];
+            sqB += vecB[i] * vecB[i];
+        }
+        
+        // Console.WriteLine(fileNameA + "-" + fileNameB + "=" + (1d - dotProd / (Math.Sqrt(sqA) * Math.Sqrt(sqB))));
+
+        return 1d - dotProd / (Math.Sqrt(sqA) * Math.Sqrt(sqB));
+    }
+
+    private static List<Lexeme> CorrectFileLexemes(List<Lexeme> lexemes)
+    {
+        var lexemesWithoutPunctuation = lexemes.Where(
+            lexeme => lexeme.LexemeText is not "{" or "}" or "[" or "]" or "(" or ")" or "," or ""
+        );
+        
+        var lexemesWithoutUselessKeywords = lexemesWithoutPunctuation.Where(
+            lexeme => lexeme.LexemeText is not "sealed" or "auto" or "ansi" or "beforefieldinit"
+                or "hidebysig" or "managed" or "valuetype" or "specialname" or "rtspecialname" 
+                or "native" or "assembly" or "only" or "nested" or "serializable" or "literal"
+                or ".maxstack" or ".locals" or ".custom"
+        );
+
+        var lexemesWithoutLabels = lexemesWithoutUselessKeywords.Where(
+            lexeme => lexeme.Kind != LexemeKind.Label ||
+                      lexeme.Kind == LexemeKind.AssemblerCommand && lexeme.LexemeText == "nop"
+        );
+
+        var lexemesWithCorrectedSharpEntities = lexemesWithoutLabels.Select(lexeme =>
+        {
+            if (lexeme.Kind != LexemeKind.Entity) return lexeme;
+            return new Lexeme
+            {
+                Kind = LexemeKind.Entity, LexemePosition = lexeme.LexemePosition,
+                LexemeText = lexeme.LexemeText
+                    .Replace("[mscorlib]", "[library]")
+                    .Replace("[System.Runtime]", "[library]")
+                    .Replace("[System.Runtime.Extensions]", "[library]")
+                    .Replace("[System.Core]", "[library]")
+                    .Replace("[System.Linq]", "[library]")
+            };
+        });
+        
+        // Console.WriteLine($"{lexemes.Count} -> {lexemesWithCorrectedSharpEntities.Count()}");
+        
+        return lexemesWithCorrectedSharpEntities.ToList();
+    }
+}
