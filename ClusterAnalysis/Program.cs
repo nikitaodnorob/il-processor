d3e2a3d6cd8f9677695325fc69f2c189fa577182
nodnorob <nodnorob@tradingview.com>
2022-05-07 22:40:13 +0300

    Refactoring of vector operations

--- ClusterAnalysis/Program.cs
+++ ClusterAnalysis/Program.cs
@@ -260,40 +260,8 @@ internal static class Program
         var vecA = LexemesBag[fileNameA];
         var vecB = LexemesBag[fileNameB];
 
-        // Console.WriteLine(fileNameA + "-" + fileNameB + "=" + CosDistance(vecA, vecB));
+        // Console.WriteLine(fileNameA + "-" + fileNameB + "=" + Vector.CosDistance(vecA, vecB));
         
-        return CosDistance(vecA, vecB);
-    }
-
-    private static double CosDistance(double[] vecA, double[] vecB)
-    {
-        double dotProd = 0;
-        double sqA = 0;
-        double sqB = 0;
-        for (int i = 0; i < vecA.Length; i++)
-        {
-            dotProd += vecA[i] * vecB[i];
-            sqA += vecA[i] * vecA[i];
-            sqB += vecB[i] * vecB[i];
-        }
-
-        return 1d - dotProd / (Math.Sqrt(sqA) * Math.Sqrt(sqB));
-    }
-
-    private static double[] Normalize(double[] vec)
-    {
-        double sqSum = 0;
-        for (int i = 0; i < vec.Length; i++)
-        {
-            sqSum += vec[i] * vec[i];
-        }
-
-        double[] res = new double[vec.Length];
-        for (int i = 0; i < vec.Length; i++)
-        {
-            res[i] = vec[i] / sqSum;
-        }
-
-        return res;
+        return Vector.CosDistance(vecA, vecB);
     }
 }
