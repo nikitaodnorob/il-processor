ad410fd41aed411359ed99a1edf3f023dbca7e7e
nikitaodnorob <nikitaodnorob@gmail.com>
2022-05-10 20:44:14 +0300

    Refactoring LexemeFilter

--- ClusterAnalysis/LexemesFilter.cs
+++ ClusterAnalysis/LexemesFilter.cs
@@ -6,6 +6,17 @@ public static class LexemesFilter
 {
     public static List<Lexeme> Filter(List<Lexeme> code)
     {
+        code = FilterPunctuation(code);
+        code = FilterUselessKeywords(code);
+        code = FilterLabels(code);
+        code = FilterLinesEnd(code);
+        code = CorrectEntities(code);
+
+        return code;
+    }
+
+    private static List<Lexeme> FilterPunctuation(List<Lexeme> code)
+    {
         var lexemesWithoutPunctuation = code.Where(lexeme => lexeme.LexemeText is not "," or "");
 
         if (Settings.DistanceMetric != DistanceMetric.Stylometry)
@@ -13,24 +24,38 @@ public static class LexemesFilter
                 lexeme => lexeme.LexemeText is not "{" or "}" or "[" or "]" or "(" or ")"
             );
 
-        var lexemesWithoutUselessKeywords = lexemesWithoutPunctuation.Where(
+        return lexemesWithoutPunctuation.ToList();
+    }
+
+    private static List<Lexeme> FilterUselessKeywords(List<Lexeme> code)
+    {
+        return code.Where(
             lexeme => lexeme.LexemeText is not "sealed" or "auto" or "ansi" or "beforefieldinit"
                 or "hidebysig" or "managed" or "valuetype" or "specialname" or "rtspecialname" 
                 or "native" or "assembly" or "only" or "nested" or "serializable" or "literal"
                 or ".maxstack" or ".locals" or ".custom"
-        );
+        ).ToList();
+    }
 
-        var lexemesWithoutLabels = lexemesWithoutUselessKeywords.Where(
-            lexeme => lexeme.Kind != LexemeKind.Label ||
-                      lexeme.Kind == LexemeKind.AssemblerCommand && lexeme.LexemeText == "nop"
-        );
+    private static List<Lexeme> FilterLabels(List<Lexeme> code)
+    {
+        return code.Where(lexeme => lexeme.Kind != LexemeKind.Label).ToList();
+    }
+    
+    private static List<Lexeme> FilterLinesEnd(List<Lexeme> code)
+    {
+        return code.Where(lexeme => lexeme.Kind != LexemeKind.LineEnd).ToList();
+    }
 
-        var lexemesWithCorrectedSharpEntities = lexemesWithoutLabels.Select(lexeme =>
+    private static List<Lexeme> CorrectEntities(List<Lexeme> code)
+    {
+        return code.Select(lexeme =>
         {
             if (lexeme.Kind != LexemeKind.Entity) return lexeme;
             return new Lexeme
             {
-                Kind = LexemeKind.Entity, LexemePosition = lexeme.LexemePosition,
+                Kind = LexemeKind.Entity,
+                LexemePosition = lexeme.LexemePosition,
                 LexemeText = lexeme.LexemeText
                     .Replace("[mscorlib]", "[library]")
                     .Replace("[System.Runtime]", "[library]")
@@ -38,10 +63,6 @@ public static class LexemesFilter
                     .Replace("[System.Core]", "[library]")
                     .Replace("[System.Linq]", "[library]")
             };
-        });
-        
-        // Console.WriteLine($"{lexemes.Count} -> {lexemesWithCorrectedSharpEntities.Count()}");
-        
-        return lexemesWithCorrectedSharpEntities.ToList();
+        }).ToList();
     }
 }
