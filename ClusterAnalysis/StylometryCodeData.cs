4c67736101d42610aa493feb2869cf1ac3c3937e
nodnorob <nodnorob@tradingview.com>
2022-05-10 20:34:24 +0300

    Add ToString for StylometryCodeData

--- ClusterAnalysis/StylometryCodeData.cs
+++ ClusterAnalysis/StylometryCodeData.cs
@@ -6,4 +6,12 @@ public struct StylometryCodeData
     public double MethodsLocalVarsAvgCnt;
     public double MethodsMaxStackAvg;
     public double LexicalDiversity;
+
+    public override string ToString()
+    {
+        return $"MethodsAvgL={Math.Round(MethodsAvgLength, 2).ToString(),-5}, " +
+               $"MethodsVarsAvgCnt={Math.Round(MethodsLocalVarsAvgCnt, 2).ToString(),-5}, " +
+               $"MethodsMaxStackAvg={Math.Round(MethodsMaxStackAvg, 2).ToString(),-5}, " +
+               $"LexDiv={Math.Round(LexicalDiversity, 4).ToString(),-6}";
+    }
 }
